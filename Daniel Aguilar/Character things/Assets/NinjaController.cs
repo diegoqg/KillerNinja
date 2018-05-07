7a3defff4e8b4d200691bd2e5c769782b8fc2bcb
danielaguilar94 <daniel.aguilar.94@hotmail.es>
2018-05-07 16:23:42 +0200

    proyecto dani

---
+++ Daniel Aguilar/Character things/Assets/NinjaController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinjaController : MonoBehaviour {
+
+    public float moveSpeed = 3f;
+    public float jumpHigh = 3f;
+    float distancia;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+    }
+
+
+    void OnMouseDown()
+    {
+
+        this.distancia = this.transform.position.z;
+
+    }
+
+    void OnMouseDrag()
+    {
+       
+        Vector3 temp = Input.mousePosition;
+        
+        //Debug.Log("mouse: ", temp.z);
+        //temp.z = this.distancia;
+        //this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+        transform.position += Vector3.up * jumpHigh * Time.deltaTime;
+    }
+
+}
