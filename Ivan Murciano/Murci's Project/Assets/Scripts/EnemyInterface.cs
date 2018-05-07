5d5e716ecc79182fb642d63bb8c2a751f9a04217
Murcis96 <ivan_1196@hotmail.es>
2018-05-07 19:37:34 +0200

    MiProyecto

---
+++ Ivan Murciano/Murci's Project/Assets/Scripts/EnemyInterface.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Enemy
+{
+    void Pegar();
+    void RecibirDaño();
+}
+
+public interface Boss
+{
+    void GeneraMapaBoss();
+}
