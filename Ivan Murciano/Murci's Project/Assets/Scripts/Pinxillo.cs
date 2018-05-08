23ab3486d137dd190f299acbfd7e30b8075dd177
Murcis96 <ivan_1196@hotmail.es>
2018-05-08 16:45:48 +0200

    Add Goblin

--- Ivan Murciano/Murci's Project/Assets/Scripts/Pinxillo.cs
+++ Ivan Murciano/Murci's Project/Assets/Scripts/Pinxillo.cs
@@ -7,28 +7,50 @@ public class Pinxillo : MonoBehaviour , Enemy{
     int Daño;    
     Transform TfPlayer;
     bool HePegado;
-    float MinDistancia = 1.0f;
+    public float MinDistancia = 2.0f;
     GameObject GoPlayer;
+    public float freq = 0.2f;    
+    private Animator anim;
+    private CharacterController controller;
+    private bool battle_state;
 
     public void RecibirDaño()
     {
-       
+        Vida = Vida - 1;
+        if (Vida == 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void Pegar()
     {
+        HePegado = true;
         Debug.Log("PEGAR A NINJA");
+        anim.SetInteger("battle", 1);
+		battle_state = true;
+        anim.SetInteger("moving", 3);
         GoPlayer.GetComponent<NinjaMove>().RecibirDaño();
+        // IdleAtack();
+    }
+
+    public void IdleAtack()
+    {
+        anim.SetInteger("battle", 0);
+        battle_state = false;
     }
 
 	// Use this for initializations
 	void Start () {
-        Daño = 1;     
+        Daño = 1;
+        Vida = 1;
         HePegado = false;
         // Buscamos el Objeto del Ninja
         GoPlayer = GameObject.FindWithTag("Ninja");
         TfPlayer = GoPlayer.transform;
-	}
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -39,7 +61,18 @@ public class Pinxillo : MonoBehaviour , Enemy{
             // Comprovamos si el ninja esta en la distancia minima para pegar
             if (Vector3.Distance(transform.position, TfPlayer.position) <= MinDistancia)
             {
-                Pegar();
+                if (!HePegado)
+                {
+                    Pegar();
+                }
+                if (HePegado)
+                {
+                    freq -= Time.deltaTime;
+                    if (freq <= 0)
+                    {
+                        IdleAtack();
+                    }                    
+                }
             }
         }                
 	}
