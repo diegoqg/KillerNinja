31eefab3ec3a7498c24bb735c0e1d9fc17365696
danielaguilar94 <daniel.aguilar.94@hotmail.es>
2018-05-08 16:48:21 +0200

    commit

---
+++ Daniel Aguilar/Character things/Assets/Scripts/NinjaAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinjaAttack : MonoBehaviour {
+
+
+    public Object weapon;
+    public float fireRate;
+    public LayerMask noHit;
+
+    private float timeToFire = 0f;
+    private Transform firePoint;
+    
+
+    // Use this for initialization
+    void Start () {
+
+        weapon = Resources.Load("shuriken");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, firePointPosition - mousePosition, 100, noHit);
+        //Instantiate(weapon, transform.position, );
+        
+        Debug.DrawLine(firePointPosition, (firePointPosition - mousePosition) * 100, Color.black);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+        }
+    }
+}
--- Daniel Aguilar/Character things/Assets/Scripts/NinjaControl.cs
+++ Daniel Aguilar/Character things/Assets/Scripts/NinjaControl.cs
@@ -6,7 +6,6 @@ public class NinjaControl : MonoBehaviour {
 
     public float moveSpeed { get; set; }
     public float jumpHigh { get; set; }
-    public Object shuriken;
     private Vector2 touchOrigin = -Vector2.one;
     private Vector2 touchFinal;
     private Vector2 distancia;
@@ -14,7 +13,6 @@ public class NinjaControl : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        shuriken = Resources.Load("shuriken");
         moveSpeed = 3f;
         jumpHigh = 10f;
     }
@@ -23,6 +21,7 @@ public class NinjaControl : MonoBehaviour {
 	void Update () {
 
         transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+
         // mayor que 0 si queremos contar mas de un dedo.
         if (Input.touchCount > 0)
         {
@@ -53,10 +52,7 @@ public class NinjaControl : MonoBehaviour {
             }
         }
 
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Instantiate(shuriken, transform.position + (transform.right * 2), transform.rotation);
-        }
+        
     }
 
 
